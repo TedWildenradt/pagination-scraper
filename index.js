5da36230d845775f6a00471e90e108c6d820516f
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-23 11:26:00 -0700

    Webscrape with pagination

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const request = require('request');
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+async function scrape() {
+  // const url = "https://sfbay.craigslist.org/search/vol?s=0"
+  for(let i = 0; i <= 365; i += 120){
+    const html = await request.get("https://sfbay.craigslist.org/search/vol?s=" + i);
+    const $ = await cheerio.load(html);
+    $(".result-title").each( (idx, el) => console.log($(el).text()) )
+    console.log("At page number " + i);
+  }
+
+}
+
+scrape();
